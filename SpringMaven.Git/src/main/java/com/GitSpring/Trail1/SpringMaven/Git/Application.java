bcf6a4881698459237a28951538a52d21e018d18
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-08-04 11:28:04 +0530

    FirstCommitGitBash

---
+++ SpringMaven.Git/src/main/java/com/GitSpring/Trail1/SpringMaven/Git/Application.java
@@ -0,0 +1,13 @@
+package com.GitSpring.Trail1.SpringMaven.Git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ SpringMaven.Git/src/test/java/com/GitSpring/Trail1/SpringMaven/Git/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GitSpring.Trail1.SpringMaven.Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
